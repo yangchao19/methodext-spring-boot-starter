76d3b3ffc74b491902d5c93a9a663e9f6aafe33c
yangchao19 <yangchao19981013@163.com>
2023-08-08 13:59:34 +0800

    update

--- src/main/java/com/yang/middleware/methodext/DoMethodExtPoint.java
+++ src/main/java/com/yang/middleware/methodext/DoMethodExtPoint.java
@@ -11,6 +11,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
@@ -21,6 +22,7 @@ import java.lang.reflect.Method;
  * @Copyright：
  */
 @Aspect
+@Component
 public class DoMethodExtPoint {
 
     private Logger logger = LoggerFactory.getLogger(DoMethodExtPoint.class);
@@ -31,11 +33,11 @@ public class DoMethodExtPoint {
     }
 
     @Around("aopPoint()")
-    public Object DoRouter(ProceedingJoinPoint joinPoint) throws Throwable {
-        // 获取内容
+    public Object doRouter(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 获取被注解的方法，和注解内容
         Method method = getMethod(joinPoint);
         DoMethodExt doMethodExt = method.getAnnotation(DoMethodExt.class);
-        // 获取拦截方法名
+        // 获取注解中的方法名
         String methodName = doMethodExt.method();
         // 功能处理
         Class<?> clazz = getClass(joinPoint);
@@ -47,7 +49,7 @@ public class DoMethodExtPoint {
             throw new RuntimeException("annotation @DoMethodExt set method:" + methodName + "returnType is no boolean");
         }
 
-        // 获取代理对象，通过反射执行注解的方法
+        // 获取代理对象，通过反射执行注解中指定方法名对应的自定义方法
         boolean invoke = (boolean)methodExt.invoke(joinPoint.getThis(), joinPoint.getArgs());
         return invoke ? joinPoint.proceed() : JSON.parseObject(doMethodExt.returnJson(), method.getReturnType());
     }
